615ae61089ef07ded69a0bd8b425af06c9c4d2a7
sam-sepiol <68191144+sam5epi0l@users.noreply.github.com>
2022-05-31 13:34:53 +0000

    Add files via upload

---
+++ js/webhook.js
@@ -0,0 +1,16 @@
+document.getElementById("send").onclick = (e) => {
+  const message = document.getElementById("message").value;
+  const email = document.getElementById("email").value;
+
+  fetch("https://davidf.tk:3000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, message }),
+  })
+    .then((r) => r.text())
+    .then((r) => {
+      document.getElementById("response").innerHTML = r;
+    });
+};
